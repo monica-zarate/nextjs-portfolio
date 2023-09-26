0fe830de9503dfa93d1db0b8367607697bbc6d2e
Monica Zarate <60247850+monica-zarate@users.noreply.github.com>
2023-09-25 17:17:07 -0700

    Fixed modal image styles. Added suspense to Navbar.

--- src/app/_components/Modal.jsx
+++ src/app/_components/Modal.jsx
@@ -16,7 +16,7 @@ export default function Modal({selected, setSelected}) {
     animate={{opacity: 1, transition: { duration: 0.5 }}}
     onClick={() => setSelected(null)}
     className="fixed inset-0 bg-black/50 z-50 cursor-pointer overflow-y-scroll">
-        <div onClick={(e) => e.stopPropagation()} className="w-full 2xl:px-0 max-w-7xl mx-auto h-screen flex items-center justify-center px-8 cursor-default">
+        <div onClick={(e) => e.stopPropagation()} className="w-full max-w-7xl mx-auto h-screen my-auto px-8 pt-28 sm:pt-32 lg:pt-24 xl:pt-20 cursor-default">
             <Image src={selected} alt="project-details"/>
             <FontAwesomeIcon icon={faXmark} style={{color: "#ffffff"}} className="absolute top-20 right-8 sm:top-24 lg:top-16 xl:top-12 cursor-pointer w-8 h-8 drop-shadow-md" onClick={() => setSelected(null)}/>
         </div>
--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -1,6 +1,6 @@
 // Vendor imports
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Suspense } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
@@ -42,21 +42,23 @@ export default function Navbar() {
 
     return (
         <header className="bg-white border-b border-solid border-neutral-100 drop-shadow sticky top-0 z-[1]">
-            <div className="flex justify-between items-center px-8 2xl:px-2 py-4 mx-auto max-w-7xl">
-                <Link href="/" className="flex items-center z-[5]">
-                    <Image src={sunflower} alt="sunflower icon" className="w-8 sm:w-10 h-8 sm:h-10 drop-shadow-md"/>
-                    <span className={`${caveat.className} text-lime-950 text-h2 sm:text-h1Light`}>{copy.title}</span>
-                </Link>
-                {isMobile && <MobileMenu/>}
-                {!isMobile && <ul className="flex">
-                    <li key={routes[0].id}>
-                        <Link href={`/${routes[0].id}`} className={`${pathname === routes[0].id ? "text-fuchsia-700" : "text-lime-950"} text-bodyBold ml-4 hover:text-fuchsia-700 delay-200 duration-200 ease-in-out uppercase`}>{routes[0].title}</Link>
-                    </li>
-                    <li key={routes[1].id}>
-                        <Link href={`/${routes[1].id}`} className={`${pathname === routes[1].id ? "text-fuchsia-700" : "text-lime-950"} text-bodyBold ml-4 hover:text-fuchsia-700 delay-200 duration-200 ease-in-out uppercase`}>{routes[1].title}</Link>
-                    </li>
-                </ul>}
-            </div>
+            <Suspense>
+                <div className="flex justify-between items-center px-8 2xl:px-2 py-4 mx-auto max-w-7xl">
+                    <Link href="/" className="flex items-center z-[5]">
+                        <Image src={sunflower} alt="sunflower icon" className="w-8 sm:w-10 h-8 sm:h-10 drop-shadow-md"/>
+                        <span className={`${caveat.className} text-lime-950 text-h2 sm:text-h1Light`}>{copy.title}</span>
+                    </Link>
+                    {isMobile && <MobileMenu/>}
+                    {!isMobile && <ul className="flex">
+                        <li key={routes[0].id}>
+                            <Link href={`/${routes[0].id}`} className={`${pathname === routes[0].id ? "text-fuchsia-700" : "text-lime-950"} text-bodyBold ml-4 hover:text-fuchsia-700 delay-200 duration-200 ease-in-out uppercase`}>{routes[0].title}</Link>
+                        </li>
+                        <li key={routes[1].id}>
+                            <Link href={`/${routes[1].id}`} className={`${pathname === routes[1].id ? "text-fuchsia-700" : "text-lime-950"} text-bodyBold ml-4 hover:text-fuchsia-700 delay-200 duration-200 ease-in-out uppercase`}>{routes[1].title}</Link>
+                        </li>
+                    </ul>}
+                </div>
+            </Suspense>
         </header>
     )
 }
